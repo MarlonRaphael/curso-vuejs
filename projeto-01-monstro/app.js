257caeafe0d5b0fca57902665d8f98acb23e5434
Marlon Raphael <marlon.raphaael@hotmail.com>
2021-10-02 20:05:31 -0300

    Finalização das implementações do projeto Matador de Monstro

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,6 +1,7 @@
 new Vue({
   el: '#app',
   data: {
+    logs: [],
     running: false,
     player1: {
       name: 'Fernanda',
@@ -25,6 +26,7 @@ new Vue({
         this.running = true
         this.player1.life = 100
         this.player2.life = 100
+        this.logs = []
       }
     },
     endGame() {
@@ -35,17 +37,33 @@ new Vue({
       }
     },
     attack(especial) {
-      this.hurt('player1', 7, 12, false)
-      this.hurt('player2', 5, 10, especial)
+      this.hurt('player2', 5, 10, especial, this.player2.name, this.player1.name, 'alert-danger')
+
+      if (this.player2.life > 0) {
+        this.hurt('player1', 7, 12, false, this.player1.name, this.player2.name, 'alert-primary')
+      }
     },
-    hurt(prop, min, max, especial) {
+    hurt(prop, min, max, especial, source, target, cls) {
       const plus = especial ? 5 : 0
       const hurt = this.getRandom(min + plus, max + plus)
       this[prop].life = Math.max(this[prop].life - hurt, 0)
+      this.registerLog(`${source} atingiu o ${target} com ${hurt}.`, cls)
+    },
+    healAndHurt() {
+      this.heal(10, 15)
+      this.hurt('player1', 7, 12, false, this.player2.name, this.player1.name, 'alert-danger')
+    },
+    heal(min, max) {
+      const heal = this.getRandom(min, max)
+      this.player1.life = Math.min(this.player1.life + heal, 100)
+      this.registerLog(`${this.player1.name} ganhou força de ${heal}.`, 'alert-success')
     },
     getRandom(min, max) {
       const value = Math.random() * (max - min) + min
       return Math.round(value)
+    },
+    registerLog(text, cls) {
+      this.logs.unshift({ text, cls })
     }
   },
   watch: {
