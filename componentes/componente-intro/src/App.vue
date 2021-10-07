185c7e7725ed9651c2182c5f40bb0ce4f8bccca3
Marlon Raphael <marlon.raphaael@hotmail.com>
2021-10-07 00:19:02 -0300

    Práticas e exercícios do capítulo Introdução Componentes

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <app-contadores></app-contadores>
+    <button>?</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ componentes/componente-intro/src/Contador.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="contador">
+    <span>{{ contador }}</span>
+    <button @click="adicionar">+</button>
+    <button @click="subtrair">-</button>
+    <div>teste</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contador",
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+    adicionar() {
+      this.contador++
+    },
+    subtrair() {
+      this.contador--
+    }
+  }
+}
+</script>
+
+<style scoped>
+  /*div {*/
+  /*  border: 1px solid blue;*/
+  /*}*/
+
+  span {
+    border-bottom: 1px solid #ccc;
+    height: 30px;
+    padding: 5px 25px;
+  }
+
+  button {
+    margin: 5px;
+    height: 30px;
+    width: 30px;
+    border-radius: 15px;
+    background-color: coral;
+    color: #fff;
+    outline: 0;
+  }
+</style>
