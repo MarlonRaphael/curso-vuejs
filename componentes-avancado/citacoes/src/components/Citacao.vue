57036766de11d1ad846c40cf0239de915a89af74
Marlon Raphael <marlon.raphaael@hotmail.com>
2021-10-10 23:28:57 -0300

    Práticas e exercícios do capítulo Componentes Avançado

---
+++ componentes-avancado/citacoes/src/components/Citacao.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="citacao">
+    <slot name="fonte"></slot>
+    <slot></slot>
+    <div class="autor">
+      <slot name="autor"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Citacao',
+  created() {
+    console.log('created')
+  },
+  destroyed() {
+    console.log('destroyed')
+  },
+  activated() {
+    console.log('activated')
+  },
+  deactivated() {
+    console.log('deactivated')
+  }
+}
+</script>
+
+<style scoped>
+  .citacao {
+    border: 1px solid #DDD;
+    background-color: rgba(0, 0, 0, .1);
+    padding: 25px;
+    max-width: 450px;
+  }
+
+  .autor {
+    float: right;
+  }
+
+  h4 {
+    color: red;
+  }
+</style>
---
+++ componentes-avancado/citacoes/src/components/Citacoes.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="citacoes">
+    <Citacao>
+<!--      <h4 slot="autor">Autor: </h4>-->
+      <h4 slot="autor">{{ citacoes[indice].autor }}</h4>
+<!--      <h4 slot="autor">{{ citacoes[indice].autor }}</h4>-->
+<!--      <p>Citação</p>-->
+      <p>{{ citacoes[indice].texto }}</p>
+<!--      <h6 slot="fonte">{{ citacoes[indice].fonte }}</h6>-->
+      <h6 slot="fonte">{{ citacoes[indice].fonte }}</h6>
+    </Citacao>
+    <span>
+      <button @click="numero--">&lt;</button>
+      <button @click="numero++">&gt;</button>
+    </span>
+  </div>
+</template>
+
+<script>
+import Citacao from "@/components/Citacao";
+
+export default {
+  components: {
+    Citacao
+  },
+  data() {
+    return {
+      numero: 0,
+      citacoes: [{
+        fonte: 'Jornal do Empreendedor',
+        texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
+        autor: 'Margaret Mead'
+      }, {
+        fonte: 'Frases de Mãe',
+        texto: 'Isso não é um quarto, é um chiqueiro.',
+        autor: 'Roberta'
+      }, {
+        fonte: 'Frases de Pai',
+        texto: 'Vou contar até 3! 1, 2, 2...',
+        autor: 'Gustavo'
+      }]
+    }
+  },
+  computed: {
+    indice() {
+      return Math.abs(this.numero % 3)
+    }
+  },
+  created() {
+    console.log('created')
+  },
+  destroyed() {
+    console.log('destroyed')
+  },
+  activated() {
+    console.log('activated')
+  },
+  deactivated() {
+    console.log('deactivated')
+  }
+}
+</script>
+
+<style scoped>
+  .citacoes {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h4 {
+    /*color: green;*/
+  }
+</style>
